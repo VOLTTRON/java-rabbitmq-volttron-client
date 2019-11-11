effe9499b866186d026891e6936c190f4dafd194
Craig A <craig.allwardt@pnnl.gov>
2019-11-11 11:37:47 -0800

    Handle missing jks and ts files better.

--- src/main/java/gov/pnnl/rabbitmq/volttron/example/Config.java
+++ src/main/java/gov/pnnl/rabbitmq/volttron/example/Config.java
@@ -9,6 +9,8 @@ public class Config {
 	public final static String instanceName = "v2";
 	// virtual host for connecting the exchange to.
 	public final static String exchangeName = "volttron";
+	
+	// Keystore and trust store paths to use.
 	public final static String keyStorePath = "/home/osboxes/repos/volttron-develop-no-py3/jackpot-keystore.jks";
 	public final static String trustStorePath = "/home/osboxes/repos/volttron-develop-no-py3/rabbitmq.ts";
 	public final static String keyStorePassword = "volttron";
--- src/main/java/gov/pnnl/rabbitmq/volttron/example/RabbitTestClient.java
+++ src/main/java/gov/pnnl/rabbitmq/volttron/example/RabbitTestClient.java
@@ -2,12 +2,40 @@ package gov.pnnl.rabbitmq.volttron.example;
 
 import com.rabbitmq.client.Connection;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class RabbitTestClient {
 	
 	public static void main(String[] args) throws Exception {
+		System.out.println("This is a rabbitmq test client, please use at your own risk\n");
+		File ksfile = new File(Config.keyStorePath);
+		boolean isValid = true;
+		
+		if (!ksfile.exists()){
+			System.err.println(String.format("Key Store Path: %s does not exist", Config.keyStorePath));
+			isValid = false;
+		}
+		
+		File tsfile = new File(Config.keyStorePath);
+				
+		if (!tsfile.exists()){
+			System.err.println(String.format("Trust Store Path: %s does not exist", Config.trustStorePath));
+			isValid = false;
+		}
+		
+		if (!isValid) {
+			System.err.println("Exiting due to missing trust and/or key store paths in \n\tgov/pnnl/rabbitmq/volttron/example/Config.java");
+			System.exit(0);
+		}
+		
+		System.out.println("Current configuration has the following attributes:");
+		System.out.println("\tTrust Store Path: "+Config.trustStorePath);
+		System.out.println("\tKey Store Path: " + Config.keyStorePath);
+		System.out.println("If this is not the correct please modify the file");
+		System.out.println("\tgov/pnnl/rabbitmq/volttron/example/Config.java");
+		System.out.println("And run the command ./gradlew jarWithDependencies from the top level of this repository.");
 		Connection conn = null;
 		RabbitConnections connections = new RabbitConnections();
 		try {
